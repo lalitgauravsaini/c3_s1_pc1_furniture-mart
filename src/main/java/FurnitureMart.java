8667048d8404031b8e8392fc7f880089c8dd21c6
lalitsaini859@gmail.com <lalitsaini859@gmail.com>
2022-08-10 06:03:20 +0530

    complete

---
+++ src/main/java/FurnitureMart.java
@@ -0,0 +1,14 @@
+package main.java;
+
+@SuppressWarnings("ALL")
+public class FurnitureMart {
+    String colours;
+    double prices;
+    String furnitureType;
+    int grades;
+
+public double discountCalculate(double percentage){
+
+    return discountCalculate(5);
+}
+}
